653da0479a4f6f376d730fb2e4f9519e5af52d85
Aditya <adityapratamax11@gmail.com>
2024-02-15 14:59:06 +0700

    Add facebook instagram and some words

--- src/app/components/molecules/HeroSection.jsx
+++ src/app/components/molecules/HeroSection.jsx
@@ -3,11 +3,11 @@ import Subtitle from '../atoms/Subtitle'
 import HeroAvatar from '../atoms/HeroAvatar'
 export default function Hero() {
     return (
-        <section id='home' className='pt-32'>
+        <section id='home' className='sm:pt-32'>
             <div className="container">
                 <div className="flex flex-wrap">
                     <div className="w-full self-center px-15 lg:px-24 lg:w-1/2">
-                        <h1 className='text-base font-semibold text-primary md:text-xl'>Halo semua, Saya <span className='block text-dark text-4xl lg:text-5xl'>Aditya Pratama</span></h1>
+                        <h1 className='text-base font-semibold text-primary md:text-xl'>Hello there, I'am <span className='block text-dark text-4xl lg:text-5xl'>Aditya Pratama</span></h1>
                         <Subtitle />
                         <p className='font-medium text-slate-400 mb-10 leading-relaxed'>keep learning, stay humble and stay grateful </p>
                         <Button />
