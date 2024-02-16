cb5b4b77af354d136f9bab9e0151ff033a35ccec
Aditya <adityapratamax11@gmail.com>
2024-02-16 16:57:16 +0700

    add navbar

--- src/app/components/atoms/Hamburger.jsx
+++ src/app/components/atoms/Hamburger.jsx
@@ -1,13 +1,9 @@
 'use client';
 import { useEffect, useState } from "react"
-export default function Hamburger() {
-    const [toggle, setToggle] = useState(false);
+export default function Hamburger(props) {
 
-    function handleClick() {
-        setToggle(!toggle)
-    }
     return (
-        <button onClick={handleClick} id="hamburger" className={`block absolute right-4 ${toggle ? "hamburger-active" : ""}`}>
+        <button onClick={props.onClick} id="hamburger" className={`lg:hidden block absolute right-4 ${props.isActive ? "hamburger-active" : ""}`}>
             <span className="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
             <span className="hamburger-line transition duration-300 ease-in-out"></span>
             <span className="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
--- src/app/components/molecules/Navbar.jsx
+++ src/app/components/molecules/Navbar.jsx
@@ -5,6 +5,11 @@ import { useState, useEffect } from 'react'
 export default function Navbar() {
     const [scrolling, setScrolling] = useState(false);
     const [scrollTop, setScrollTop] = useState(0)
+    const [toggle, setToggle] = useState(false);
+
+    const hideNavbar = () => {
+        setToggle(!toggle)
+    }
 
     useEffect(() => {
         function onScroll() {
@@ -19,14 +24,36 @@ export default function Navbar() {
         document.addEventListener('scroll', onScroll)
     }, [])
     return (
-        <header className={`bg-transparent ${scrolling ? "navbar-fixed" : ""} top-0 left-0 w-full flex items-center z-10`}>
+        <header className={`bg-transparent absolute ${scrolling ? "navbar-fixed" : ""} top-0 left-0 w-full flex items-center z-10`}>
             <div className="container">
                 <div className="flex items-center justify-between relative">
                     <div>
                         <a href="#home" className="font-bold text-lg text-primary block py-6">Aditya Pratama</a>
                     </div>
                     <div className="flex items-center px-4">
-                        <Hamburger />
+                        <Hamburger onClick={hideNavbar} isActive={toggle} />
+                        <nav id='nav-menu' className={`${toggle ? '' : ''} absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none`}>
+                            <ul className='block lg:flex'>
+                                <li className='group'>
+                                    <a href="#home" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Home</a>
+                                </li>
+                                <li className='group'>
+                                    <a href="#about" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>About Me</a>
+                                </li>
+                                <li className='group'>
+                                    <a href="#portfolio" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Portfolio</a>
+                                </li>
+                                <li className='group'>
+                                    <a href="#clients" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Clients</a>
+                                </li>
+                                <li className='group'>
+                                    <a href="#blog" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Blog</a>
+                                </li>
+                                <li className='group'>
+                                    <a href="#contact" className='text-base text-dark py-2 mx-8 flex group-hover:text-primary'>Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
