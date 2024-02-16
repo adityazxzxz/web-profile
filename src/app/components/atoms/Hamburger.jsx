56fb8acc1b1fac951ebd6f15e6f890c2ea8f64dd
Aditya <adityapratamax11@gmail.com>
2024-02-16 17:58:44 +0700

    fixing toggle nav hidden on change screen

--- src/app/components/atoms/Hamburger.jsx
+++ src/app/components/atoms/Hamburger.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export default function Hamburger(props) {
 
     return (
-        <button onClick={props.onClick} id="hamburger" className={`lg:hidden block absolute right-4 `}>
+        <button onClick={props.onClick} id="hamburger" className={`lg:hidden block absolute right-4 ${props.isActive ? "hamburger-active" : ""}`}>
             <span className="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
             <span className="hamburger-line transition duration-300 ease-in-out"></span>
             <span className="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
